3e378bcaaebfacfd92d3b62ceca1775d27485ae5
John Felleman <john.felleman@gmail.com>
2013-08-12 17:36:12 -0400

    Add clarifying comment on sample code

--- help-me-sam/help_me_sam/popup.js
+++ help-me-sam/help_me_sam/popup.js
@@ -1,4 +1,5 @@
 // Copyright (c) 2013
 
-$("#h2").text("Welcome to the SAM helper");
-// $("#pageheader").text( $(".page_heading").val);
+// This is how one might modify the popup tht is displayed when clicking on the page action
+// icon in the address bar
+// $("#h2").text("Welcome to the SAM helper");
