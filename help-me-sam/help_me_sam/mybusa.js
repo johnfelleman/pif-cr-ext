91a595a07961c775029af8b75ae781c576404863
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-12 17:23:01 -0400

    Semi-working top bar overlay

--- help-me-sam/help_me_sam/mybusa.js
+++ help-me-sam/help_me_sam/mybusa.js
@@ -1,24 +1,50 @@
-// c 2013 
+$(document).ready(function() {
+    // define the main DIVs
+    var sam_div = $('#UIPortalApplication');
+    var busa_main_div = $('<div class="busa" id="busa-main"><h1 id="dont-panic">DON\'T PANIC</h1><div id="busa-content"></div></div>');
+    var busa_toggle_div = $('<div class="busa"><p><a href="#" id="busa-toggle">Close</a></p></div>');
 
-console.log("I am logging like crazy");
-function load_help_window() {
-  var w = window.open("chrome-extension://fkelfaihobjpfilfiifbhjchdmcjialm/help-for-page.html",
-	      "",
-	      "status=no, height=800, width=650, resizable=no, toolbar=no, menubar=no, scrollbars=no, location=no, directories=no"
-	      );
-  // w.onload=function() { $("#helptitle", w.document).text("this will be something else"); };
-  console.log("there is an open window");
-  $(document).ready( function() { console.log("this is the doc ready handler speaking"); console.log("current helptitle: " + $("body", w.document.body)) ;  $("#helptitle").text("this will be something else"); });
-}
+    // insert the DIVs into the DOM
+    //var xhr = new XMLHttpRequest();
+    ////xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
+    //xhr.open("GET", chrome.extension.getURL("./busa-main.html"), true);
+    //xhr.send();
+    //console.log(xhr);
 
-$("#dataTable tbody tr").on("click", function(event){
-  alert($(this).text());
-  });
+//$('#busa-content').load(chrome.extension.getURL("./busa-main.html"));
+    console.log(chrome.extension.getURL("./busa-main.html"));
+    busa_main_div.hide();
+    busa_main_div.insertBefore(sam_div);
+    busa_toggle_div.insertAfter(busa_main_div);
 
-  
-var buttontext=document.createTextNode("Help for " +  $("div[class=page_heading]").text() + "...");
-buttontext.id="helptext";
-var helpbutton=document.createElement("BUTTON");
-helpbutton.appendChild(buttontext);
-helpbutton.addEventListener("click", load_help_window, false);
-$("#SAMBanner").after(helpbutton);;
+    $.getJSON('http://mo.tynsax.com/api/registrations/1/full_map')
+        .done(function( json ) {
+            var page_name = json['name'].toString();
+            console.log(page_name);
+            $('div#busa-content').append(page_name);
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            alert("Oh noes!! The call to modus-operandi failed!");
+        });
+
+    // define toggle/click/slide behavior
+    $('#busa-toggle').click(function() {
+        if ( busa_main_div.is(":visible") ) {
+            busa_main_div.slideUp();
+
+            // contextual page info
+            var page_name = $("div.page_heading").text();
+            if (!page_name) {
+                page_name = 'SAM';
+            }
+            
+            $(this).text("Get Help for " + page_name);
+        } else {
+            busa_main_div.slideDown();
+            $(this).text("Close");
+        }
+    });
+
+    // trigger opening animation
+    busa_main_div.slideDown();
+});
