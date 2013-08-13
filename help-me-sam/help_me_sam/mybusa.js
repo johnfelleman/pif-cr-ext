82476f32148e44a80b711fe62658bbec8f5d99be
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-13 12:05:25 -0400

    Got context-sensitive content grabbing/templating working

--- help-me-sam/help_me_sam/mybusa.js
+++ help-me-sam/help_me_sam/mybusa.js
@@ -4,6 +4,33 @@ $(document).ready(function() {
     var busa_main_div = $('<div class="busa" id="busa-main"><div id="busa-content"></div></div>');
     var busa_toggle_div = $('<div class="busa"><p><a href="#" id="busa-toggle">Close</a></p></div>');
 
+    //console.log(chrome.extension.getURL("busa-main.html"));
+    //$('div#busa-content').load($.open(chrome.extension.getURL("busa-main.html")));
+    busa_main_div.hide();
+    busa_main_div.insertBefore(sam_div);
+    busa_toggle_div.insertAfter(busa_main_div);
+
+    // contextual page info
+    var page_name = $("div.page_heading").text();
+    if (!page_name) {
+        page_name = 'SAM.gov';
+    }
+    console.log("Page Name: " + page_name);
+    var div_id = page_name.replace(/ /g, '-');
+    div_id = div_id.replace(/\./g, '-');
+    console.log("DIV ID: " + div_id);
+
+    // pull API data from modus-operandi
+    $.getJSON('http://mo.tynsax.com/api/registrations/1/full_map')
+        .done(function(json) {
+            //page_name = json['name'].toString();
+            //$('div#busa-content').append(page_name);
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            alert("Oh noes!! The call to modus-operandi failed!");
+        });
+
+
     // insert the DIVs into the DOM
     var req = $.ajax({
         type: "GET",
@@ -11,38 +38,16 @@ $(document).ready(function() {
         dataType: "html"
     });
     req.done(function(msg) {
-        $('div#busa-content').html(msg);
+        $('div#busa-content').html($(msg).filter("#" + div_id));
     });
     req.fail(function(jqXHR, textStatus) {
         console.log("Request failed: " + textStatus);
     });
 
-    //console.log(chrome.extension.getURL("busa-main.html"));
-    //$('div#busa-content').load($.open(chrome.extension.getURL("busa-main.html")));
-    busa_main_div.hide();
-    busa_main_div.insertBefore(sam_div);
-    busa_toggle_div.insertAfter(busa_main_div);
-
-    $.getJSON('http://mo.tynsax.com/api/registrations/1/full_map')
-        .done(function( json ) {
-            var page_name = json['name'].toString();
-            console.log(page_name);
-            $('div#busa-content').append(page_name);
-        })
-        .fail(function(jqxhr, textStatus, error) {
-            alert("Oh noes!! The call to modus-operandi failed!");
-        });
-
     // define toggle/click/slide behavior
     $('#busa-toggle').click(function() {
         if ( busa_main_div.is(":visible") ) {
             busa_main_div.slideUp();
-
-            // contextual page info
-            var page_name = $("div.page_heading").text();
-            if (!page_name) {
-                page_name = 'SAM';
-            }
             
             $(this).text("Get Help for " + page_name);
         } else {
