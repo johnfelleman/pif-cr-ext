ee34417c05f666d8237e5215ea63d56410bff6e9
JohnFelleman <JohnFelleman@JohnFellemans-MacBook-Air.local>
2013-09-06 13:14:47 -0400

    simple validation mostly working

--- help-me-sam/help_me_sam/mybusa.js
+++ help-me-sam/help_me_sam/mybusa.js
@@ -31,12 +31,24 @@ $(document).ready(function() {
     // So we do this instead
     // Each of these objects should have come from JSON
     // Object properties:
-    //		"matchString" : <string>	<=> value of 'title' attribute in page
-    //		"required" : "true"|"false"	<=> indicates whether this field is required
-    //		"valid"	: <string>		<=> free text describing valid input (will become schema)
-    //		"hover" : <string>		<=> text to display when mouse hovers over field
-    //		"focused" : <string>		<=> text to display when the field has focus
-    //		"unfocused" : <string>		<=> text to display when focus is lost (will be validation)
+    //   "matchString" : <string>	<=> value of 'title' attribute in page
+    //   "required" : "true"|"false"	<=> indicates whether this field is required
+    //   "valid"	: <string>		<=> free text describing valid input (will become schema)
+    //   "hover" : <string>		<=> text to display when mouse hovers over field
+    //   "focused" : <string>		<=> text to display when the field has focus
+    //   "unfocused" : <string>		<=> text to display when focus is lost (will be validation)
+
+    //   "validator" : <object>		<=> validation requirement object
+    //       "type" : <string>		<=> type of data: "number" | "string" | "email" | etc.
+    //       "rules" : <object>		<=> if required, validation rules object (see below)
+
+    //       "rules" : <object>			<=> alphabetic string validator rules
+    //            "type" : "alpha-string"	<=> must match validator type
+    //		  "length" : <maxlength>	<=> maximum string length
+
+    //       "rules" : <object>			<=> confirmation of another field
+    //            "type" : "confirm"		<=> must match validator type
+    //		  "primary" : <string>		<=> "matchString" value of field to match
 
     title = {
 	matchString: "Title",
@@ -44,50 +56,131 @@ $(document).ready(function() {
 	valid: "Any pull-down value",
 	hover: "Your address title",
 	focus: "Choose a value",
-	unfocused: "Oops, your title is invalid"
+	unfocused: "Oops, your title is invalid",
+	validator: {
+	    type: "none",
+	    rules: null
+	}
     };
     first_name = {
 	matchString: "First Name",
 	required: "true",
-	hover: "you are on First Name!",
-	valid: "No spaces",
+	hover: "Your legal first or given name",
+	valid: "alphabetic characters only\n14 character maximum",
 	focus: "Enter your name",
-	unfocused: "don't you know your own name?"
+	unfocused: "don't you know your own name?",
+	validator: {
+	    type: "alpha-string",
+	    rules: {
+		type: "alpha-string",
+		length: "14"
+	    }
+	}
     };
     middle_initial = {
 	matchString: "Middle Initial",
 	required: "false",
 	valid: "one letter only, no punctuation",
-	hover: "your middle initial, please",
+	hover: "Your middle initial",
 	focus: "enter your initial",
-	unfocused: "one letter only please"
+	unfocused: "one letter only please",
+	validator: {
+	    type: "none",
+	    rules: null
+	}
     };
     last_name = {
 	matchString: "Last Name",
 	required: "true",
-	valid: "No spaces or punctuation",
-	hover: "Your legal last name",
+	hover: "Your legal last or family name",
+	valid: "alphabetic characters only\n14 character maximum",
 	focus: "please enter your family name",
-	unfocused: "Oops, your name is invalid"
+	unfocused: "Oops, your name is invalid",
+	validator: {
+	    type: "alpha-string",
+	    rules: {
+		type: "alpha-string",
+		length: "14"
+	    }
+	}
     };
     suffix = {
 	matchString: "Suffix",
 	required: "false",
 	valid: "any characters",
-	hover: "Do you use a suffix after your name",
-	focus: "go ahead enter it now",
-	unfocused: "look okay to me"
+	hover: "Suffix",
+	focus: "Enter a suffix if desired",
+	unfocused: "",
+	validator: {
+	    type: "none",
+	    rules: null
+	}
     };
     email_address = {
 	matchString: "Email Address",
 	required: "true",
 	valid: "must be a legal email address",
-	hover: "this is your contact email address",
+	hover: "Primary contact email address",
 	focus: "please enter your email address",
-	unfocused: "this is not a valid email"
+	unfocused: "this is not a valid email",
+	validator: {
+	    type: "email"
+	}
+    };
+    email_confirmation = {
+	matchString: "Confirm Email Address",
+	required: "true",
+	valid: "must match email address",
+	hover: "Confirm your contact email address",
+	focus: "please enter your email address",
+	unfocused: "this is not a match",
+	validator: {
+	    type: "email"
+	}
+    };
+    phone = {
+	matchString: "Phone",
+	required: "true",
+	hover: "Your primary phone number",
+	valid: "10 digit us phone number",
+	focus: "Enter your phone number",
+	unfocused: "Oops, your name is invalid",
+	validator: {
+	    type: "us-phone",
+	    rules: {
+		type: "us-phone"
+	    }
+	}
+    };
+    extension = {
+	matchString: "Phone Extension",
+	required: "false",
+	hover: "extension",
+	valid: "anything",
+	focus: "Enter any necessary extension",
+	unfocused: "",
+	validator: {
+	    type: "none",
+	    rules: null
+	}
+    };
+    fax = {
+	matchString: "Fax",
+	required: "false",
+	hover: "Fax number",
+	valid: "10 digit us phone number",
+	focus: "Enter your fax number",
+	unfocused: "",
+	validator: {
+	    type: "us-phone",
+	    rules: {
+		type: "us-phone"
+	    }
+	}
     };
     
-    var fields = [ title, first_name, middle_initial, last_name, suffix, email_address ];
+    var fields = [ title, first_name, middle_initial, last_name, suffix,
+		    email_address, email_confirmation, phone, extension, fax ];
 
     for (var index = 0; index < fields.length; index++) {
 	$('input[title="' + fields[index].matchString + '"]').mouseenter(
@@ -132,10 +225,57 @@ $(document).ready(function() {
 	    $('p.field-focus').html('info for active field:<br>' + field.focus);
 	});
 
+	// Do validation when leaving field
 	$('input[title="' + fields[index].matchString + '"]').focusout(
 		    { field: fields[index]},function(ev) {
 	    var field = ev.data.field;
-	    $('p.field-focus').html("");
+	    var userEntered = this.value;
+	    var errorOccurred = 'false';
+	    var errorMessage = "Error:\n";
+	    switch (field.validator.type) {
+		case "alpha-string":
+		if (this.value.indexOf(" ") != -1) {
+		    errorMessage += "no spaces allowed\n";
+		    errorOccurred = 'true';
+		}
+		if (userEntered.length > field.validator.rules.length) {
+		    errorMessage += "maximum length: " + field.validator.rules.length;
+		    errorOccurred = 'true';
+		}
+		if (errorOccurred == 'true') {
+		    alert(errorMessage);
+		    this.focus();
+		    this.select();
+		}
+		break;
+
+		case "us-phone":
+		var digitsOnly = this.value.replace(/\D/g,"");
+		if (digitsOnly.length != 10) {
+		    alert(errorMessage + "Telephone numbers must have 10 digits");
+		    this.focus();
+		    this.select();
+		} else {
+		    this.value = '(' + digitsOnly.substr(0,3) + ')'
+			   +  digitsOnly.substr(3,3) + '-' + digitsOnly.substr(6,4);
+		}
+		break;
+
+		case "email":
+		if (!this.value.match( /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)) {
+		    alert(errorMessagej + this.value + ' is not a valid email address');
+		}
+		break;
+
+		case "confirm":
+		if (!this.value.match( /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)) {
+		    alert(errorMessagej + this.value + ' is not a valid email address');
+		}
+		break;
+
+		default:
+		break;
+	    }
 	});
 
     }
