d2bbc4839b0e78b9ea7246579c519a4f8d1e4ae9
JohnFelleman <JohnFelleman@JohnFellemans-MacBook-Air.local>
2013-09-07 18:11:02 -0400

    Now using JSON file to specify fields

--- help-me-sam/help_me_sam/mybusa.js
+++ help-me-sam/help_me_sam/mybusa.js
@@ -1,234 +1,28 @@
 // Create a USDS helper library namespace
 
 var usdsJsHelper = {};
-(function(context) { 
-    var id = 0;
 
-    context.next = function() {
-	return id++;    
-    };
+// This will be cool someday when I am smarter
+// (function(context) { 
+    // var id = 0;
+
+    // context.next = function() {
+	// return id++;    
+    // };
       
-    context.reset = function() {
-	id = 0;     
-    }
-})(usdsJsHelper);  
+    // context.reset = function() {
+	// id = 0;     
+    // }
+// })(usdsJsHelper);  
 
-window.console && console.log(
-usdsJsHelper.next(),
-usdsJsHelper.next(),
-usdsJsHelper.reset(),
-usdsJsHelper.next()
-) //0, 1, undefined, 0
+// window.console && console.log(
+// usdsJsHelper.next(),
+// usdsJsHelper.next(),
+// usdsJsHelper.reset(),
+// usdsJsHelper.next()
+// ) //0, 1, undefined, 0
 
-// In HTML5, use session storage to set/get the open/minimized state of the helper
-// If not HTML5, always return minimized (this could be improved with a SESSION cookie)
-    
-$(document).ready(function() {
-
-    // read from the MO server just to prove we can
-    chrome.runtime.sendMessage({command: "get-json-from-url",
-		url: "http://mo.tynsax.com/api/operations "}, function(response) {
-      // console.log(response.farewell);
-      workFlow = response;
-      alert(JSON.stringify(workFlow));
-      });
-
-    // use the background page to get the JSON
-    var fieldListFromJson;
-    var moWorkflow;
-    chrome.runtime.sendMessage({command: "get-json-from-url", url: "field_list.json"}, function(response) {
-      fieldListFromJson = response;
-      alert(JSON.stringify(fieldListFromJson));
-      });
-
-    // $( document ).ajaxError(function() {
-      // $( "div.log" ).text( "Triggered ajaxError handler." );
-      // alert('failed to complete AJAX request');
-      // });
-
-    // bgPage =  chrome.runtime.getBackgroundPage()
-    // var jsonUrl = chrome.extension.getURL('field_list.json');
-    // alert('file is ' + jsonUrl);
-    // bgPage.getJSON(jsonUrl, function(data) {
-    	// alert('got some JSON');
-	// items = data.list;
-	// // $.each(items, function(key, val) {
-	    // alert('key: ' + key + ' value: ' + value);
-	    // });
-	// });
-
-    if (sessionStorage.getItem("visible") == null) {
-    	sessionStorage.setItem("visible", 'true');
-    }
-
-    // define the main DIVs
-    var sam_div = $('#UIPortalApplication');
-    var busa_main_div = $('<div class="busa" id="busa-main"><div id="busa-content"></div></div>');
-    var busa_toggle_div =
-	$('<div class="busa"><p><a href="#" id="busa-toggle">Minimize the SAM Helper</a></p></div>');
-    var floating_help_div =
-	$('<div class="floating-help-box" id="display-hover-text" style="z-index:20000">hat</div>');
-    busa_main_div.insertBefore(sam_div);
-    busa_toggle_div.insertAfter(busa_main_div);
-    floating_help_div.insertAfter(busa_toggle_div);
-
-    // The code below should read some info from a local JSON file and build the field handlers
-    // ...but it doesn't...at least not yet
-
-    // So we do this instead
-    // Each of these objects should have come from JSON
-    // Object properties:
-    //   "matchString" : <string>	<=> value of 'title' attribute in page
-    //   "required" : "true"|"false"	<=> indicates whether this field is required
-    //   "valid"	: <string>		<=> free text describing valid input (will become schema)
-    //   "hover" : <string>		<=> text to display when mouse hovers over field
-    //   "focused" : <string>		<=> text to display when the field has focus
-    //   "unfocused" : <string>		<=> text to display when focus is lost (will be validation)
-
-    //   "validator" : <object>		<=> validation requirement object
-    //       "type" : <string>		<=> type of data: "number" | "string" | "email" | etc.
-    //       "rules" : <object>		<=> if required, validation rules object (see below)
-
-    //       "rules" : <object>			<=> alphabetic string validator rules
-    //            "type" : "alpha-string"	<=> must match validator type
-    //		  "length" : <maxlength>	<=> maximum string length
-
-    //       "rules" : <object>			<=> confirmation of another field
-    //            "type" : "confirm"		<=> must match validator type
-    //		  "primary" : <string>		<=> "matchString" value of field to match
-
-    title = {
-	matchString: "Title",
-	required: "false",
-	valid: "Any pull-down value",
-	hover: "Your address title",
-	focus: "Choose a value",
-	unfocused: "Oops, your title is invalid",
-	validator: {
-	    type: "none",
-	    rules: null
-	}
-    };
-    first_name = {
-	matchString: "First Name",
-	required: "true",
-	hover: "Your legal first or given name",
-	valid: "alphabetic characters only\n14 character maximum",
-	focus: "Enter your name",
-	unfocused: "don't you know your own name?",
-	validator: {
-	    type: "alpha-string",
-	    rules: {
-		type: "alpha-string",
-		length: "14"
-	    }
-	}
-    };
-    middle_initial = {
-	matchString: "Middle Initial",
-	required: "false",
-	valid: "one letter only, no punctuation",
-	hover: "Your middle initial",
-	focus: "enter your initial",
-	unfocused: "one letter only please",
-	validator: {
-	    type: "none",
-	    rules: null
-	}
-    };
-    last_name = {
-	matchString: "Last Name",
-	required: "true",
-	hover: "Your legal last or family name",
-	valid: "alphabetic characters only\n14 character maximum",
-	focus: "please enter your family name",
-	unfocused: "Oops, your name is invalid",
-	validator: {
-	    type: "alpha-string",
-	    rules: {
-		type: "alpha-string",
-		length: "14"
-	    }
-	}
-    };
-    suffix = {
-	matchString: "Suffix",
-	required: "false",
-	valid: "any characters",
-	hover: "Suffix",
-	focus: "Enter a suffix if desired",
-	unfocused: "",
-	validator: {
-	    type: "none",
-	    rules: null
-	}
-    };
-    email_address = {
-	matchString: "Email Address",
-	required: "true",
-	valid: "must be a legal email address",
-	hover: "Primary contact email address",
-	focus: "please enter your email address",
-	unfocused: "this is not a valid email",
-	validator: {
-	    type: "email"
-	}
-    };
-    email_confirmation = {
-	matchString: "Confirm Email Address",
-	required: "true",
-	valid: "must match email address",
-	hover: "Confirm your contact email address",
-	focus: "please enter your email address",
-	unfocused: "this is not a match",
-	validator: {
-	    type: "email"
-	}
-    };
-    phone = {
-	matchString: "Phone",
-	required: "true",
-	hover: "Your primary phone number",
-	valid: "10 digit us phone number",
-	focus: "Enter your phone number",
-	unfocused: "Oops, your name is invalid",
-	validator: {
-	    type: "us-phone",
-	    rules: {
-		type: "us-phone"
-	    }
-	}
-    };
-    extension = {
-	matchString: "Phone Extension",
-	required: "false",
-	hover: "extension",
-	valid: "anything",
-	focus: "Enter any necessary extension",
-	unfocused: "",
-	validator: {
-	    type: "none",
-	    rules: null
-	}
-    };
-    fax = {
-	matchString: "Fax",
-	required: "false",
-	hover: "Fax number",
-	valid: "10 digit us phone number",
-	focus: "Enter your fax number",
-	unfocused: "",
-	validator: {
-	    type: "us-phone",
-	    rules: {
-		type: "us-phone"
-	    }
-	}
-    };
-    
-    var fields = [ title, first_name, middle_initial, last_name, suffix,
-		    email_address, email_confirmation, phone, extension, fax ];
-
+usdsJsHelper.loadFieldHandlers = function(fields) {
     for (var index = 0; index < fields.length; index++) {
 	$('input[title="' + fields[index].matchString + '"]').mouseenter(
 		    { field: fields[index]},function(ev) {
@@ -326,6 +120,30 @@ $(document).ready(function() {
 	});
 
     }
+}
+
+$(document).ready(function() {
+
+    // define the main DIVs
+    var sam_div = $('#UIPortalApplication');
+    var busa_main_div = $('<div class="busa" id="busa-main"><div id="busa-content"></div></div>');
+    var busa_toggle_div =
+	$('<div class="busa"><p><a href="#" id="busa-toggle">Minimize the SAM Helper</a></p></div>');
+    var floating_help_div =
+	$('<div class="floating-help-box" id="display-hover-text" style="z-index:20000">hat</div>');
+    busa_main_div.insertBefore(sam_div);
+    busa_toggle_div.insertAfter(busa_main_div);
+    floating_help_div.insertAfter(busa_toggle_div);
+
+    if (sessionStorage.getItem("visible") == null) {
+    	sessionStorage.setItem("visible", 'true');
+    }
+
+    // use the background page to get the JSON
+    var fields;
+    chrome.runtime.sendMessage({command: "get-json-from-url", url: "field_list.json"}, function(response) {
+      usdsJsHelper.loadFieldHandlers(response.result);
+      });
 
     // contextual page info
     var page_name = $("div.page_heading").text();
@@ -338,15 +156,6 @@ $(document).ready(function() {
     var div_id = page_name.replace(/ /g, '-');
     div_id = div_id.replace(/\./g, '-');
     console.log("DIV ID: " + div_id);
-
-    // pull API data from modus-operandi
-    // $.getJSON('http://mo.tynsax.com/api/registrations/1/full_map')
-    //     .done(function(json) {
-    //         //page_name = json['name'].toString();
-    //         //$('div#busa-content').append(page_name);
-    //     })
-    //     .fail(function(jqxhr, textStatus, error) {
-    //     });
 
     // insert the DIVs into the DOM
     var req = $.ajax({
