4fba4f785d1e86842bd0f0df1449b432cbca7518
John Felleman <john.felleman@gsa.gov>
2013-09-18 16:18:09 -0400

    fixed some display bugs, added masking

--- help-me-sam/help_me_sam/samHelper.js
+++ help-me-sam/help_me_sam/samHelper.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
     // Get the field data (for now, from a file, someday from MO server)
     $.ajax({
         type: "GET",
-        url: chrome.extension.getURL("field_list.json"),
+        url: chrome.extension.getURL("samHelper.json"),
         dataType: "json"
     }).done(function(msg) {
       usdsJsHelper.loadFieldHandlers(pageToken, msg);
@@ -44,7 +44,7 @@ $(document).ready(function() {
     overviews = {
         "SAM_gov":
         "<p>Welcome to the SAM Helper.  This utility will guide you through the process of registering with SAM, so that you can do business with the government.</p><p>To begin, click the 'Create User Account' button in the leftmost box below.</p>",
-        "Create_Account":
+        "Create_an_Account":
         "<p>To begin, you must create a user account with SAM.  Start that process by clicing the 'Individual Account' button below on the left.</p>",
         "Personal_Information":
         "<p>The first step in creating your user account is to collect some personal information.  Please go ahead and fill out the screen below.  Make sure to complete the required items marked with a '*'.  The helper will assist you in entering valid information.</p>"
@@ -54,7 +54,7 @@ $(document).ready(function() {
     var pageContent;
     $.ajax({
         type: "GET",
-        url: chrome.extension.getURL("busa-main.html"),
+        url: chrome.extension.getURL("samHelper.html"),
         dataType: "html"
     }).done( function(data) {
         pageContent = usdsJsHelper.contentForPgItm(data, pageToken);
@@ -92,30 +92,29 @@ $(document).ready(function() {
         alert('failed to read page content');
     });
 
-
-    // get some random stuff (this needs to be refactored out of existence
-    // req = $.ajax({
-        // type: "GET",
-        // url: chrome.extension.getURL("busa-main.html"),
-        // dataType: "html"
-    // }).done(function(msg) {
-        // $('div#busa-content').html($(msg).filter("#" + pageToken));
-        // $(msg).filter('#help_info').insertAfter($('div#busa-content'));
-// 
-        // $('dd#button-me').html( '<button id="view-workflow" name="show-mo" type="button">View</button>');
-        // $('dd#button-me').click(function() {
-            // $(window.open('', 'workflow',
-                // 'location=no, height=600, width=600').document.body).html(sessionStorage.processMap);
-        // });
-        // $('dd#wiz-me').html( '<button id="launch-wizard" name="launch-wizard" type="button">Take me to the Wizard</button>');
-        // $('dd#wiz-me').click(function() {
-        // $(window.open('', 'wizard',
-            // 'location=no, height=600, width=600').document.body).html("I will be your wizard");
-        // });
-    // }).fail(function(jqXHR, textStatus) {
-        // alert('failed to read some random stuff');
-    // });
-
+    // you could even blank out wrong choices!
+    if (pageToken === 'Create_an_Account') {
+        $('div.sub_heading').text("");
+        $('div.search_entity_results_div2').filter(function() {
+              return $(this).css('float') == 'right';
+        }).remove();
+        /* this version would put an alpha mask over the square
+        var hideSysAccount = $('<div class="mask-this">xxxx</div>');
+        $(hideSysAccount).css({
+            "position": "absolute",
+            "border": "2px solid blue",
+            "padding": "0px",
+            "margin": "0px",
+            "background-color": "rgba(0,0,128,0.6)",
+            "width":"inherit",
+            "height": "inherit"
+        });
+        var itemToHide = $('div.search_entity_results_div2').filter(function() {
+                          return $(this).css('float') == 'right';
+                                         });
+        itemToHide.prepend($(hideSysAccount));
+         */
+    }
     // read the JSON workflow data
     sessionStorage.processMap = '{}';
     $.getJSON('http://mo.tynsax.com/api/operations/sam-system-of-award-management-registration-process/map',
